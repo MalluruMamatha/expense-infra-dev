30bb46fc2d79fefaa3b0fb0d9e465d542b725314
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-23 21:14:46 +0530

    Jenkins

--- 02-sg/sg.tf
+++ 02-sg/sg.tf
@@ -273,3 +273,15 @@ resource "aws_security_group_rule" "web_alb_public_https" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
 }
+
+
+### jenkins cicd
+
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
